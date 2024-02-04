cc115a079e969f461dbb0188c5fdfd619914637f
mustafa <msolmaz83@gmail.com>
2024-02-04 22:39:08 +0100

    register

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,8 +1,35 @@
 
+import { Link } from "react-router-dom"
+import registerImg from "../../assets/register.png"
+import styles from "./auth.module.scss"
+import Card from "../../components/card/Card"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <section className={`container ${styles.auth}`}>
+    <Card> 
+   
+    <div className={styles.form}>
+      <h2>Register</h2>
+      <form>
+        <input type="text" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <input type="password" placeholder="Confirm Password" required />
+        <button className="--btn --btn-primary --btn-block">Register</button>
+      
+
+      </form>
+    
+      <span className={styles.register}>
+        <p>Already an account?</p>
+        <Link to="/login"> Login</Link>
+      </span>
+    </div>
+    </Card>
+    <div className={styles.img}>
+      <img src={registerImg} alt="login" width="400" />
+    </div>
+  </section>
   )
 }
 
