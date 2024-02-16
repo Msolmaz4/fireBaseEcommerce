c9e1f19bec5f410b272cb177c10a6e69d6426ff7
mustafa <msolmaz83@gmail.com>
2024-02-16 18:43:08 +0100

    home_fertig

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -2,14 +2,42 @@ import "./Slider.scss"
 import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai"
 
 import { sliderData } from "./slider-data"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 const Slider = () => {
     const [cur ,setCur] = useState(0)
+    const slideLength = sliderData.length
+    const nextSlide = ()=>{
+        setCur(cur === slideLength - 1 ?   0 :  cur + 1)
+    }
+    const prevSlide =()=>{
+        setCur(cur === 0  ?   slideLength - 1:  cur - 1)
+    }
+ 
+useEffect(()=>{
+    setCur(0)
+},[])
+const autoScroll = true
+let  slideInterval;
+let intervalTime= 5000;
+
+
+
+useEffect(() => {
+    if (autoScroll) {
+      const auto = () => {
+        slideInterval = setInterval(nextSlide, intervalTime);
+      };
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [cur, slideInterval, autoScroll]);
+
+
   return (
     <div className="slider">
 
-        <AiOutlineArrowLeft className="arrow prev"/>
-        <AiOutlineArrowRight className="arrow next"/>
+        <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide}/>
+        <AiOutlineArrowRight className="arrow next" onClick={nextSlide}/>
         {
             sliderData?.map((slide,index)=>{
                 const {heading,desc,image} = slide
