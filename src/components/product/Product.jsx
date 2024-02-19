b8505e7e49fc5735bc5355c3120d50ad1481a651
mustafa <msolmaz83@gmail.com>
2024-02-19 22:17:46 +0100

    data_product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,28 +1,21 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { STORE_PRODUCTS } from "../../redux/slice/productSlice";
-
+import { selectProducts } from "../../redux/slice/productSlice";
 import useFetchCollection from "../../customHooks/useFetchCollection";
 
 const Product = () => {
   const { data } = useFetchCollection();
 
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(
-      STORE_PRODUCTS({
-        products: data,
-      })
-    );
-  }, []);
+  const products = useSelector(selectProducts);
+  console.log(products);
 
   return (
-    
-      <div>hallo</div>
-   
+    <div>
+      {products?.map((pro) => (
+        <div>{pro.id}</div>
+      ))}
+    </div>
   );
-}
+};
 
 export default Product;
--- src/customHooks/useFetchCollection.jsx
+++ src/customHooks/useFetchCollection.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { db } from "../firebase/config";
 import axios from "axios"
+import { useDispatch } from "react-redux";
+import { STORE_PRODUCTS } from "../redux/slice/productSlice";
 
 
 const useFetchCollection = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
 
   const getCollection =async () => {
     const veri = await axios.get("https://dummyjson.com/products");
     const products = veri.data?.products;
- 
+   
    
 console.log(products)
     setIsLoading(true);
@@ -25,12 +28,15 @@ console.log(products)
   //     }
  //burda dikkat et bir kere yukleyince kapat yoksa surekli yukler ve bu hataya sebep olabilir
      const querySnapshot = await getDocs(collection(db, "products"));
-     querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-  });
-   
+     const fetchedData = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
 
+   
+    setData(fetchedData);
+    console.log(data,"data")
+   
 
 
     } catch (error) {
@@ -41,7 +47,16 @@ console.log(products)
 
   useEffect(() => {
     getCollection();
-  }, []);
+    const derleme = async ()=>{
+       await     dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    )
+    }
+    derleme()
+    
+  }, [dispatch]);
 
   return { data, isLoading };
 };
