cc072abbeef2a800e6ad7f45d2115bf35364dce5
mustafa <msolmaz83@gmail.com>
2024-02-21 18:15:54 +0100

    isActive_category

--- src/pages/product/Products.jsx
+++ src/pages/product/Products.jsx
@@ -1,54 +1,66 @@
+import { useState } from "react";
+import useFetchCollection from "../../customHooks/useFetchCollection";
 
-import useFetchCollection from "../../customHooks/useFetchCollection"
+import styles from "./Product.module.scss";
+const initialCategories = [
+  { name: "All", isActive: true },
+  { name: "Laptop", isActive: false },
+  { name: "Electronic", isActive: false },
+  { name: "Fashion", isActive: false },
+  { name: "Phone", isActive: false },
+];
 
-import styles from "./Product.module.scss"
- const categor =[
-    "All","Laptop","Electronic","Fashion","Phone"
- ]
- const sele= []
+const sele = [];
 
 const Products = () => {
-    const {data} = useFetchCollection()
-    console.log(data,"eeeeeeeeeeeeee")
-    const catt = data
-    console.log(catt,"catttttttttttt")
- 
-  
+  const { data } = useFetchCollection();
+  const [categor, setCategor] = useState(initialCategories);
+  console.log(data, "eeeeeeeeeeeeee");
+
   return (
     <div>
-    <div className={styles.product1}>       <div className={styles.product11}> Categories</div>
-<div className={styles.product12}>2parca</div></div>
-   <div className={styles.product2}> <div className={styles.product22}>
+      <div className={styles.product1}>
+        {" "}
+        <div className={styles.product11}> Categories</div>
+        <div className={styles.product12}>2parca</div>
+      </div>
+      <div className={styles.product2}  >
+        {" "}
+        <div className={styles.product22}>
+          {categor.map((ert, index) => (
+            <div 
+              key={index}
+              onClick={() => {
+                const updatedCategories = categor.map((st) =>
+                st.name === ert.name ? { ...st, isActive: !st.isActive } : { ...st, isActive: false }
+                );
+                setCategor(updatedCategories);
+                console.log(updatedCategories);
+              }}
+            >
+            <div style={{color: ert.isActive == true ? "red" :"black"}}> 
+               {ert.name}
+            </div>
+           
+            </div>
+          ))}
 
-{categor?.map((ert)=>{
-    return(
-       <div onClick={()=>console.log(ert)}> 
-       
-       {ert} </div> 
-    ) 
-})}
-
-{data?.map((cat, index) => (
-    <div key={index}>
-
-    {(!sele.includes(cat.category)) && sele.push(cat.category)}
+          {data?.map((cat, index) => (
+            <div key={index}>
+              {!sele.includes(cat.category) && sele.push(cat.category)}
+            </div>
+          ))}
+          <select name="" id="">
+            <option value="">all</option>
+            {sele?.map((er) => (
+              <option>{er} </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.product23}>4parca</div>
+      </div>
     </div>
-))}
-<select name="" id="">
-    <option value="">all</option>
-    {sele?.map((ert)=> <option>{ert} </option>)}
-</select>
+  );
+};
 
-   </div>
-<div className={styles.product23}>4parca
-
-
-     
-</div></div>
-
-
-    </div>
-  )
-}
-
-export default Products
+export default Products;
